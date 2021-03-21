851f710edead5017fe4b848933ecf8a995e1a6c6
Ivanka Dimitrova <73659747+vanchela@users.noreply.github.com>
2021-03-21 16:01:02 +0200

    Delete AnimalSpecieRepository.java

--- ProjectAnimalShelter/src/main/java/com/example/project/repository/AnimalSpecieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.repository;
-
-import com.example.project.model.entities.AnimalSpecie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnimalSpecieRepository extends JpaRepository<AnimalSpecie,Long> {
-}
