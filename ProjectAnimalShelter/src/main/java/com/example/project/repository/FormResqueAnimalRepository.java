77a2fcc3a266465730ca421960338121b4d0affd
Ivanka Dimitrova <73659747+vanchela@users.noreply.github.com>
2021-03-21 16:00:48 +0200

    Delete FormResqueAnimalRepository.java

--- ProjectAnimalShelter/src/main/java/com/example/project/repository/FormResqueAnimalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.repository;
-
-import com.example.project.model.entities.FormResqueAnimal;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FormResqueAnimalRepository extends JpaRepository<FormResqueAnimal,Long> {
-}
