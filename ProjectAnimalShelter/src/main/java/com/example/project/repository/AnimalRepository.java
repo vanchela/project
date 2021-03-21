b28a2be92e70ff7202e96ec9dad58571fa2b5488
Ivanka Dimitrova <73659747+vanchela@users.noreply.github.com>
2021-03-21 16:00:44 +0200

    Delete AnimalRepository.java

--- ProjectAnimalShelter/src/main/java/com/example/project/repository/AnimalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.repository;
-
-import com.example.project.model.entities.Animal;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnimalRepository extends JpaRepository<Animal,Long> {
-}
